80171457d0d99a48e2bb3c523a06467696d67679
5eb565f19756e40b84a32246 <gsieling@pinnacle21.com>
2020-07-27 09:45:01 -0400

    maybe

--- fix.sh
+++ fix.sh
@@ -1,5 +1,5 @@
-sed -i 's/_wp_generated\///g' page2.html
-sed -i 's/_wp_generated\///g' page3.html
-sed -i 's/_wp_generated\///g' page4.html
-sed -i 's/_wp_generated\///g' page5.html
-sed -i 's/_wp_generated\///g' index.html
+sed -i '' 's/_wp_generated[/]//g' page2.html
+sed -i '' 's/_wp_generated[/]//g' page3.html
+sed -i '' 's/_wp_generated[/]//g' page4.html
+sed -i '' 's/_wp_generated[/]//g' page5.html
+sed -i '' 's/_wp_generated[/]//g' index.html
